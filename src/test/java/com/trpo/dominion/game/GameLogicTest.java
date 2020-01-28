074235b0a0efa626cb0405b6c7dd5c3b239e8e7e
vaddya <vadik.dyachkov@gmail.com>
2020-01-28 23:29:54 +0300

    fix

--- src/test/java/com/trpo/dominion/game/GameLogicTest.java
+++ src/test/java/com/trpo/dominion/game/GameLogicTest.java
@@ -1,8 +1,11 @@
 package com.trpo.dominion.game;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameLogicTest {
+    @Test
     void test() {
         assertEquals(4, 2 + 2);
     }
