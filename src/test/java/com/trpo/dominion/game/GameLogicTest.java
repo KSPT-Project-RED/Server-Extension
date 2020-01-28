16993f33274b23c03ec16b1acbf923b011e3cf15
vaddya <vadik.dyachkov@gmail.com>
2020-01-28 23:25:15 +0300

    fix pom

---
+++ src/test/java/com/trpo/dominion/game/GameLogicTest.java
@@ -0,0 +1,9 @@
+package com.trpo.dominion.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GameLogicTest {
+    void test() {
+        assertEquals(4, 2 + 2);
+    }
+}
